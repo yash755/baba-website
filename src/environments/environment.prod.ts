d7b2e78d9dc13e721991b470e52c6aee6e0cfd7b
Yash Gupta <yash@BMT-LAP-157.local>
2025-07-27 10:50:25 +0530

    New Dist folder

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://api.jaibabamohanram.in/',
+  apiUrl: 'https://api.jaibabamohanram.co.in/',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://api.jaibabamohanram.in/',
+  apiUrl: 'https://www.api.joinpublicgroups.com/',
 };
 
 /*
